83ba01737eaef8befa95d9c349ec34d00162ce1c
Sheriff332 <remotellsyou@gmail.com>
2025-03-28 00:58:26 +0530

    finish tdoc

--- twelve_days_of_christmas/src/main.rs
+++ twelve_days_of_christmas/src/main.rs
@@ -1,21 +1,22 @@
 fn main() {
     let lines = [" twelve drummers drumming,"," eleven pipers piping,"," ten lords a-leaping,"," nine ladies dancing,"," eight maids a-milking,"," seven swans a-swimming,"," six geese a-laying,"," five gold rings,"," four calling birds,"," Three French hens,"," Two turtle doves, And"];
-    
-    for i in 0..11 {
+    let ordinal = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"];
+
+    for i in 0..12 {
         let mut middle_part = String::new();
         for j in 0..i {
             middle_part = format!("{}{}",lines[10-j],middle_part);
         }
-        println!("The first day of Christmas my true love sent to me{} a partridge in a pear-tree", middle_part);
+        println!("On the {} day of Christmas my true love sent to me{} a partridge in a pear-tree",ordinal[i], middle_part);
     }
 }
 
-//the poem can be divided into 3 parts
+//the poem can be divided into 4 parts
 
-//|The first day of Christmas my true love sent to me | | | | a partridge in a pear-tree|
+//|On The | |first| | day of Christmas my true love sent to me | | | | a partridge in a pear-tree|
 
-//|The second day of Christmas my true love sent to me | |two turtle-doves,| | a partridge in a pear-tree|
+//|On The | |second| | day of Christmas my true love sent to me | |two turtle-doves,| | a partridge in a pear-tree|
 
-//|The third day of Christmas my true love sent to me | |three fat hens, two turtle-doves,| | a partridge in a pear-tree|
+//|On The | |third| | day of Christmas my true love sent to me | |three fat hens, two turtle-doves,| | a partridge in a pear-tree|
 
 //we can write the poem by changing only the middle bit
