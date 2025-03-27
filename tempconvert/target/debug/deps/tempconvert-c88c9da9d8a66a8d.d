7d484b3706c7d54768b0becdfa8bde39ff4429cd
Sheriff332 <remotellsyou@gmail.com>
2025-03-28 00:48:20 +0530

    finish tempconvert

---
+++ tempconvert/target/debug/deps/tempconvert-c88c9da9d8a66a8d.d
@@ -0,0 +1,5 @@
+C:\Users\remot\OneDrive\Documents\Rust Practice\tempconvert\target\debug\deps\libtempconvert-c88c9da9d8a66a8d.rmeta: src\main.rs
+
+C:\Users\remot\OneDrive\Documents\Rust Practice\tempconvert\target\debug\deps\tempconvert-c88c9da9d8a66a8d.d: src\main.rs
+
+src\main.rs:
