b01cdf76f6a290cf947b2fcad109579035760cab
Sheriff <66024246+Sheriff332@users.noreply.github.com>
2025-03-28 12:01:19 +0530

    Delete tempconvert/target directory

--- tempconvert/target/debug/deps/tempconvert.d
+++
@@ -1,5 +0,0 @@
-C:\Users\remot\OneDrive\Documents\Rust Practice\tempconvert\target\debug\deps\tempconvert.exe: src\main.rs
-
-C:\Users\remot\OneDrive\Documents\Rust Practice\tempconvert\target\debug\deps\tempconvert.d: src\main.rs
-
-src\main.rs:
